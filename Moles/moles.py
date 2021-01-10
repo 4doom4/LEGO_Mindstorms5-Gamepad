b572579de4a90a84b13c1f27e2b0cf2103260f27
Maximilian Ebert <max.ebert@me.com>
2021-01-09 23:21:54 -0500

    - beta version

--- Moles/moles.py
+++ Moles/moles.py
@@ -2,12 +2,20 @@
 
 """Moles for 51515 GamePad
 
-<insert moles descriptions>
+Frank likes his garden but moles do too. They dig molehills so Frank has to
+stomp on them to retain control of his property. But be cautious! When Frank
+is sourrounded by moles jumping out of their molehill Frank is trapped and
+looses!
 
 Playing:
-<insert how to play>
+Use the left/right and center button to move up. To move down press left and
+right at the same time. When a molehill is bright like Frank he can't stomp
+on it any more.
+
+First level requires 3, second 5 and third level 8 molehills.
 
     Changelog:
+    09-jan-2020 (me) first 3 levels for beta
     01-jan-2020 (me) added moles and winning condition
     30-dec-2020 (me) created
 """
@@ -69,9 +77,9 @@ class Player():
         return l[-1:]+l[:-1]
 
     def _occupied(self, pos, moleCoords):
-        [row, col] = divmod(pos[0][0], 10)
+        [x, y] = self._rot(list(divmod(pos[0][0], 10)))
         for moleCoord in moleCoords:
-            if moleCoord == [row, col]:
+            if moleCoord == [x, y]:
                 return 1
         return 0
 
@@ -113,6 +121,8 @@ class Level():
         self.levelScore = levelScore
         self.digTime = digTime
         self.spawnTime = spawnTime
+        self.win = 0
+        self.loose = 0
 
         self._hub = MSHub()
         self._score = 0
@@ -134,16 +144,16 @@ class Level():
 
     def didPlayerBlock(self):
         [x, y] = self.player.coord()
-        # print("{}: {}".format(playerCoord, hub.display.pixel(playerCoord[1], playerCoord[0])))
+        left = []
         playerEnv = [
+            hub.display.pixel(x + 1 if x + 1 < 5 else 0, y),
+            hub.display.pixel(x - 1 if x - 1 >= 0 else 4, y),
             hub.display.pixel(x, y + 1 if y + 1 < 5 else 0),
-            hub.display.pixel(x + 1 if x+1 < 5 else 0, y + 1 if y+1 < 5 else 0),
-            hub.display.pixel(x, y - 1 if y - 1 < 0 else 4),
-            hub.display.pixel(x - 1 if y-1 < 0 else 4, y - 1 if y-1 < 0 else 4)
+            hub.display.pixel(x, y - 1 if y - 1 >= 0 else 4)
         ]
 
-        # if all(intensity == 9 for intensity in playerEnv):
-        #     self._score = -1
+        if all(intensity == 9 for intensity in playerEnv):
+           self.loose = 1
 
     def moleBlockCoord(self):
         return [mole.coord() for mole in self.moles if mole.intensity() == '9']
@@ -155,14 +165,13 @@ class Level():
         occupiedCoords = self.moleCoords()
         occupiedCoords.append(self.player.coord())
         newMole = Moles(occupiedCoords)
-        if newMole == None:
-            self._score = -1
+        if newMole.coord() == None:
+            self.loose = 1
         else:
             self.moles.append(newMole)
 
     def molesDig(self):
         [mole.dig() for mole in self.moles]
-        self.didPlayerBlock()
 
     def draw(self):
             # draw player
@@ -178,6 +187,8 @@ class Level():
             [xM, yM] = mole.coord()
             if [xP, yP] == [xM, yM]:
                 self._score += 1
+                if self._score >= self.levelScore:
+                    self.win = 1
                 hub.led(238, 246, 246)
                 hub.sound.beep(260, 200)
                 hub.led(255, 70, 35)
@@ -189,53 +200,45 @@ class Level():
         hub.display.show(hub.Image(convert_image(image)))
 
     def didLoose(self):
-        if self._score == -1:
-            print("Game Over")
-            hub.led(255,0,0)
-            sleep(2)
-            return 1
-        else:
-            return 0
+        print("Game Over")
+        hub.led(255,0,0)
+        sleep(2)
 
     def didWin(self):
-        if self._score >= self.levelScore:
-            print("Game Won")
-            hub.led(0,255,0)
-            sleep(2)
-            return 1
-        else:
-            return 0
+        print("Game Won")
+        hub.led(0,255,0)
+        sleep(2)
 
 class Game():
     def __init__(self):
-        self.totalLevels = 2
-        self.curLevel = 1
+        self.curLevel = 0
+        self.active = 1
 
-        self._spawnTime = [1, 0.4]
-        self._digTime = [0.4, 0.2]
-        self._levelScore = [3,5]
+        self._spawnTime = [1, 0.4, 0.2]
+        self._digTime = [0.4, 0.2, 0.2]
+        self._levelScore = [3,5,8]
+        self._totalLevels = len(self._spawnTime) - 1
 
     def spawnTime(self):
-        return self._spawnTime[self.curLevel-1]
+        return self._spawnTime[self.curLevel]
 
     def digTime(self):
-        return self._digTime[self.curLevel-1]
+        return self._digTime[self.curLevel]
 
     def levelScore(self):
-        return self._levelScore[self.curLevel-1]
+        return self._levelScore[self.curLevel]
 
-    def levels(self):
-        while self.curLevel <= self.totalLevels:
-            yield self.curLevel
-            self.curLevel += 1
-
+    def nextLevel(self):
+        self.curLevel += 1
+        print([self.curLevel, self._totalLevels])
+        if self.curLevel > self._totalLevels:
+            self.active = 0
 
 def moles():
     print("Welcome to Moles")
 
     game = Game()
-
-    for level in game.levels():
+    while game.active:
 
         level = Level(game.digTime(), game.spawnTime(), game.levelScore())
         level.draw()
@@ -265,20 +268,24 @@ def moles():
                 elif pressed == [0,0,1]:
                     level.playerUp()
 
-            if level.didWin():
-                print('WON!')
-                break
+            if not i%(level.digTime/0.2):
+               level.molesDig()
+            if not i%(level.spawnTime/0.2):
+               level.moleCreate()
             
-            # if not i%(level.digTime/0.2):
-            #     level.molesDig()
-            # if not i%(level.spawnTime/0.2):
-            #     level.moleCreate()
+            if level.loose:
+                level.didLoose()
+                game.active = 0
+                break
+
             level.draw()
-            level.didPlayerBlock()
-            
-            if level.didLoose():
+            if level.win:
+                level.didWin()
                 break
-            else:
-                sleep(0.2)
+
+            level.didPlayerBlock()
+
+            sleep(0.2)
+        game.nextLevel()
 
 moles()
